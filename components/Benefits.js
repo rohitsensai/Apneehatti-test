341d244cbedfd655b7b369b9703b8753e63e5761
rohitsensai <satputerohit00@gmail.com>
2024-04-26 19:56:12 +0530

    added pad

--- components/Benefits.js
+++ components/Benefits.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Benefits() {
   return (
 
-      <div className='d-flex justify-content-center lg:px-40 sm:px-20 md:px-20'>
+      <div className='d-flex justify-content-center mx-4'>
           <img
             src="/images/pictures/benefits.png" className='h-60 w-full rounded'/>
             </div>
