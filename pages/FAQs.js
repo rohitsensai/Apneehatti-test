83e967aae8486ccdae24b18b03a85497b5098611
rohitsensai <satputerohit00@gmail.com>
2024-05-19 21:10:13 +0530

    modified faq

--- pages/FAQs.js
+++ pages/FAQs.js
@@ -1,30 +1,33 @@
 import { Accordion } from "flowbite-react";
 import Head from "next/head";
 import { HiOutlineArrowCircleDown } from "react-icons/hi";
-import TextEditorView from "../components/textEditorView";
+import { useState } from "react";
 
-export async function getServerSideProps({ req }) {
+export async function getServerSideProps() {
   const data = await fetch(`${process.env.HOST}/api/faq/get`);
   const res = await data.json();
 
+
   return {
     props: {
-      faqs: res,
+      faqs: res || [],
     },
   };
 }
 
 const FAQ = ({ faqs }) => {
+  const [faqList, setFaqList] = useState(faqs);
+
   return (
     <>
       <Head>
         <title>FAQ's</title>
       </Head>
       <div className="w-full bg-gray-100 min-h-screen">
-        <div className=" lg:max-w-6xl bg-white mx-auto p-5  min-h-screen">
+        <div className="lg:max-w-6xl bg-white mx-auto p-5 min-h-screen">
           <div className="text-center space-y-3">
             <h1 className="text-sm font-medium capitalize">FAQs</h1>
-            <h1 className="text-4xl font-serif">Frequently Asked questions</h1>
+            <h1 className="text-4xl font-serif">Frequently Asked Questions</h1>
             <h5 className="text-md font-normal text-gray-500">
               Have questions? We are here to help you
             </h5>
@@ -35,19 +38,31 @@ const FAQ = ({ faqs }) => {
               alwaysOpen={true}
               arrowIcon={HiOutlineArrowCircleDown}
             >
-              {faqs?.length > 0 &&
-                faqs.map((item) => (
-                  <Accordion.Panel key={item._id}>
-                    <Accordion.Title className="font-bold">
-                      {item.question}
-                    </Accordion.Title>
-                    <Accordion.Content>
-                      {/* <p className="mb-2 text-gray-500 dark:text-gray-400">
-                        <TextEditorView desc={item.answer} />
-                      </p> */}
-                    </Accordion.Content>
-                  </Accordion.Panel>
-                ))}
+              {faqList?.length > 0 &&
+                faqList.map((item) => {
+                  // Check if answer is defined before parsing
+                  let answerText = item.answer ;
+
+                  if(answerText !== 'undefined') {
+                  
+                    answerText = JSON.parse(answerText).blocks[0].text
+                  }else{
+                    answerText = 'Answer not Available'
+                  }
+
+                  return (
+                    <Accordion.Panel key={item._id}>
+                      <Accordion.Title className="font-bold">
+                        {item.question}
+                      </Accordion.Title>
+                      <Accordion.Content>
+                        <p className="mb-2 text-gray-500 dark:text-gray-400">
+                          {answerText}
+                        </p>
+                      </Accordion.Content>
+                    </Accordion.Panel>
+                  );
+                })}
             </Accordion>
           </div>
         </div>
---
+++ testing.js
@@ -0,0 +1,7 @@
+const data = {"blocks":[{"key":"cdvhe","text":"Smile all cookies do not have eggs. ","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":0,"length":36,"style":"bgcolor-rgb(255,255,255)"},{"offset":0,"length":36,"style":"fontsize-small"},{"offset":0,"length":36,"style":"fontfamily-Arial, Helvetica, sans-serif"},{"offset":0,"length":9,"style":"color-rgb(44,130,201)"},{"offset":0,"length":10,"style":"UNDERLINE"},{"offset":9,"length":27,"style":"color-rgb(34,34,34)"}],"entityRanges":[{"offset":0,"length":9,"key":0}],"data":{}}],"entityMap":{"0":{"type":"LINK","mutability":"MUTABLE","data":{"url":"https://www.apneehatti.com/search?q=cookies","targetOption":"_self"}}}}
+
+
+console.log("data.blocks",data.blocks[0].text)
+// const res = JSON.parse(data).blocks
+
+console.log(data)
