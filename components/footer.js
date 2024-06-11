27b1822f5bab6117344837d20ae8ae1a81c4d1f9
rohitsensai <satputerohit00@gmail.com>
2024-06-11 14:41:03 +0530

    added about us page

--- components/footer.js
+++ components/footer.js
@@ -34,7 +34,7 @@ const Footer = ({ categories }) => {
         <FaArrowUp size={36} />
       </div> */}
       <footer className="footer">
-        <div className="containerfooter">
+        <div className="containerfooter bg-white rounded">
           <div className="footer_inner">
             <div className="c-footer">
               <div className="layout">
