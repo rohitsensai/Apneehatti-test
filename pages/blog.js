b2db09cb2ca1e320a3b8e9fd0b2e3bc455651b33
rohitsensai <satputerohit00@gmail.com>
2024-06-12 14:12:40 +0530

    blog responsived

--- pages/blog.js
+++ pages/blog.js
@@ -6,27 +6,27 @@ const BlogLayout = () => {
         <div className='text-4xl text-center'>
             Apneehatti's Blog 
         <div className='border  mx-4 d-flex flex-wrap justify-content-between p-2'>
-            <div className='min-h-[500px] w-[500px] border'>
+            <div className='min-h-[500px]  border flex-grow-1 min-w-[350px]'>
             <div className="bg-image text-white" style={{ backgroundImage: `url('/images/blogs/4.png')`, height: '500px', backgroundSize: 'cover', backgroundPosition: 'center' }}>Blog</div>
 
 
             </div>
-            <div className='min-h-[500px] w-[500px] border'>
-                <div className='min-h-[250px] border'>
+            <div className='min-h-[500px]  border flex-grow-1 '>
+                <div className='min-h-[250px] border min-w-[350px]'>
                 <div className="bg-image" style={{ backgroundImage: `url('/images/blogs/1.png')`, height: '250px', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
                 </div >
                 <div className='min-h-[250px] border  d-flex flex-wrap p-2 justify-content-between'>
-                    <div className='border h-[250px] w-[400px] sm:w-[238px]'>
+                    <div className='border h-[250px] flex-grow-1 min-w-[350px]'>
                     <div className="bg-image" style={{ backgroundImage: `url('/images/blogs/2.png')`, height: '250px', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
                                 
                     </div>
-                    <div className='border h-[250px] w-[400px] sm:w-[238px]'> 
+                    <div className='border h-[250px] flex-grow-1 min-w-[350px]'> 
                     <div className="bg-image text-white" style={{ backgroundImage: `url('/images/blogs/6.png')`, height: '250px', backgroundSize: 'cover' }}>Blog </div>
 
                     </div>
                 </div>
             </div>
-            <div className="min-h-[500px] w-[500px] border">
+            <div className="min-h-[500px] border flex-grow-1 min-w-[350px]">
             <div className="bg-image" style={{ backgroundImage: `url('/images/blogs/5.png')`, height: '500px', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
 
             </div>
